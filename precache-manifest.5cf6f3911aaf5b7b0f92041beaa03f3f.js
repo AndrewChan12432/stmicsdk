ee9c43e9e998b4f2e102fc47dd0b1c913b3b1701
AndrewChan12432 <55532769+AndrewChan12432@users.noreply.github.com>
2020-03-01 13:02:59 +0800

    Updates

---
+++ precache-manifest.5cf6f3911aaf5b7b0f92041beaa03f3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25db8a25ff10cb33c6318d1e551649cf",
+    "url": "/stmicsdk/index.html"
+  },
+  {
+    "revision": "b6697cc64784c8fbc332",
+    "url": "/stmicsdk/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b1095c2f6ad5b3ed7495",
+    "url": "/stmicsdk/static/js/2.1f122c9c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/stmicsdk/static/js/2.1f122c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6697cc64784c8fbc332",
+    "url": "/stmicsdk/static/js/main.f71ef851.chunk.js"
+  },
+  {
+    "revision": "41fbf986627e12611ece",
+    "url": "/stmicsdk/static/js/runtime-main.3636f5f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stmicsdk/static/media/logo.5d5d9eef.svg"
+  }
+]);
