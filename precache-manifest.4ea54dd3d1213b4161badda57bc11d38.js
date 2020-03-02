ffde7ee771c015a953f974b9f3215e2326be8d82
AndrewChan12432 <55532769+AndrewChan12432@users.noreply.github.com>
2020-03-02 08:50:46 +0800

    Updates

---
+++ precache-manifest.4ea54dd3d1213b4161badda57bc11d38.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b73df5996a69f45b20d072a42a937b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "893e9f9f72a6b7aca798",
+    "url": "/static/css/main.61e94e14.chunk.css"
+  },
+  {
+    "revision": "6b5711db22a0cdf07722",
+    "url": "/static/js/2.66ee3a04.chunk.js"
+  },
+  {
+    "revision": "0d2320288227308a2855900744658c5e",
+    "url": "/static/js/2.66ee3a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893e9f9f72a6b7aca798",
+    "url": "/static/js/main.d0d76db1.chunk.js"
+  },
+  {
+    "revision": "7d6a9e0af03f8a48074f",
+    "url": "/static/js/runtime-main.0d7cdbae.js"
+  },
+  {
+    "revision": "7df7fb3474b1e8f819985cd7ad7969cd",
+    "url": "/static/media/al.7df7fb34.png"
+  },
+  {
+    "revision": "d2363cdf53f8ed3836815b8cefaafeb0",
+    "url": "/static/media/church.d2363cdf.jpg"
+  },
+  {
+    "revision": "e72deb869035bda9323b02da0d2540fe",
+    "url": "/static/media/cottom.e72deb86.jpg"
+  },
+  {
+    "revision": "0a11273313e0602984ec1ec45d4955b4",
+    "url": "/static/media/download.0a112733.jpg"
+  },
+  {
+    "revision": "3e3c995d193188c8b189345d84648601",
+    "url": "/static/media/header.3e3c995d.png"
+  },
+  {
+    "revision": "4d955a63c46e98cc1e63352a2b555af8",
+    "url": "/static/media/mtmichael.4d955a63.jpg"
+  },
+  {
+    "revision": "53ecfccd6ff217713bc35a6d8f00d8b3",
+    "url": "/static/media/past_REACH.53ecfccd.jpg"
+  },
+  {
+    "revision": "dbdb6f0eaeb34abe6af0e0e1324e94a1",
+    "url": "/static/media/past_highlight.dbdb6f0e.jpg"
+  },
+  {
+    "revision": "fef6af160cdce4afdb8f49ba59c4a114",
+    "url": "/static/media/past_marriage.fef6af16.jpg"
+  },
+  {
+    "revision": "6e964e719d2f9514a4778d14cc8ed87c",
+    "url": "/static/media/pastor.6e964e71.jpg"
+  },
+  {
+    "revision": "f3dec4c4a67680d46d2c66dc88fa7617",
+    "url": "/static/media/stmiclogo_redcolor460x155-2.f3dec4c4.png"
+  },
+  {
+    "revision": "aacd5ee583774468a9d8ff828cbce728",
+    "url": "/static/media/workship.aacd5ee5.jpg"
+  }
+]);
